dedd1856af4efe0be1004471e293ec3b5eaa1fa8
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2022-08-02 20:15:34 +0530

    Add product part pending

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -237,12 +237,165 @@ const getSellerInfotoSellerProfile = async (req,res) => {
             seller : foundSeller,
             message : 'no Items'
             })   
+    }   
+}
+
+
+//update SellerDetails
+const updateSellerDetails = async (req,res) => {
+
+    const {name,cities_id,email,sellerConfirmPassword,sellerCurrentPassword} = req.body
+
+    const sellerImage = req.file
+    let sellerImg
+
+    if(sellerImage){
+        sellerImg =sellerImage.path
     }
 
+    if(!name || !cities_id ||  !email || !sellerCurrentPassword  )
+        return res.status(400).json({'message': 'All information are required'})
 
+    const token = req.cookies.jwt
+    let sellerEML 
+        
+    if(token){
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decodedToken) => {
+            if(err){
+                return res.status(400).json({ 'message' : 'jwt error'})
+            }else{
+                sellerEML = decodedToken.email
+            }
+        })
+    }else{
+        res.redirect('/login');
+    }
+    
+    if(!sellerEML) return res.status(400).json({ 'message' : 'User not logged in'})
 
-     
-   
+    const foundSeller = await Seller.findOne({
+        where: {
+            sellerEmail : sellerEML
+        }
+    })
+
+    if(!sellerConfirmPassword){
+        if(!foundSeller) 
+            return res.status(400).json({'message': 'No such seller'})
+        else{
+            //evaluate password
+            const match = await bcrypt.compare(sellerCurrentPassword, foundSeller.sellerPassword);
+            if(match){
+                const updSeller = await Seller.update({
+                    sellerName : sellerName,
+                    sellerContact : sellerContact,
+                    sellerCity: sellerCity,
+                    sellerEmail: sellerEmail
+                },{
+                    where: {
+                        sellerId : foundSeller.sellerId
+                    }
+                })
+                //updating sellerImage
+                if(sellerImage){
+                    const foundImage = await SellerImage.findOne({
+                        where:{
+                            sellerId : foundSeller.sellerId,
+                            status: 1
+                        }
+                    })
+
+                    if(!foundImage){
+                        const newImage = await SellerImage.create({
+                            sellerId: foundSeller.sellerId,
+                            imageName: sellerImg,
+                            status: 1
+                        })
+                    }else{
+                        const updateImage = await SellerImage.update({  
+                            status: 0
+                        },{where: {
+                                sellerId: foundSeller.sellerId,
+                                status: 1
+                            }
+                        })
+
+                        const newImage = await SellerImage.create({
+                            sellerId: foundSeller.sellerId,
+                            imageName: sellerImg,
+                            status: 1
+                        })       
+                    }
+                }
+                
+                const tkn = accessToken(sellerEmail)
+                res.cookie('jwt', tkn, {httpOnly: true, maxAge: maxAge*1000})
+                res.status(400).json({'message': 'Details Updated'})
+            }else{ 
+                res.status(400).json({'message': 'Current Password is incorrect'})
+            }
+        }
+    }else{
+        if(!foundSeller) 
+            return res.status(400).json({'message': 'No such seller'})
+        else{
+            //evaluate password
+            const match = await bcrypt.compare(sellerCurrentPassword, foundSeller.sellerPassword);
+            if(match){
+
+                const hashedPwd = await bcrypt.hash(sellerConfirmPassword, 10)
+
+                const updSeller = await Seller.update({
+                    sellerName : sellerName,
+                    sellerContact : sellerContact,
+                    sellerCity: sellerCity,
+                    sellerEmail: sellerEmail,
+                    sellerPassword : hashedPwd
+                },{
+                    where: {
+                        sellerId : foundSeller.sellerId
+                    }
+                })
+                //updating Seller Image
+                if(sellerImage){
+                    const foundImage = await SellerImage.findOne({
+                        where:{
+                            sellerId : foundSeller.sellerId,
+                            status: 1
+                        }
+                    })
+
+                    if(!foundImage){
+                        const newImage = await SellerImage.create({
+                            sellerId: foundSeller.sellerId,
+                            imageName: sellerImg,
+                            status: 1
+                        })
+                    }else{
+                        const updateImage = await SellerImage.update({  
+                            status: 0
+                        },{where: {
+                                sellerId: foundSeller.sellerId,
+                                status: 1
+                            }
+                        })
+
+                        const newImage = await SellerImage.create({
+                            sellerId: foundSeller.sellerId,
+                            imageName: sellerImg,
+                            status: 1
+                        })       
+                    }
+                }
+
+                const tkn = accessToken(sellerEmail)
+                res.cookie('jwt', tkn, {httpOnly: true, maxAge: maxAge*1000})
+                res.status(400).json({'message': 'Details Updated'})
+            }else{ 
+                res.status(400).json({'message': 'Current Password is incorrect'})
+            }
+        }
+    }   
 }
 
 module.exports = {
