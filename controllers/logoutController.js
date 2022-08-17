df1a59c3d1a9c7f7da52941f9728de846e74f8a3
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2022-08-17 19:28:25 +0530

    done

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -7,6 +7,8 @@ const Seller = db.sellers
 
 //logout
 const logout = async(req,res)=>{
+
+    req.email = null
     res.cookie('jwt','',{ maxAge: 1 })
     // res.redirect('/')
     res.status(200).json({ 'message' : 'Logout successfull'})
